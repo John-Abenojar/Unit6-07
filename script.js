84261805720bd7f44acee5be110e1a003325c475
JohnAbenojar <john.kirby.abenojar@ocsbstudent.ca>
2021-01-08 15:49:45 +0000

    L14 |     Number 1 : <input type="text" id="firstNumber" /><br>                                              ^ The id and class attribute values must be in lowercase and split by a dash. (id-class-value)       L15 |     Number 2: <input type="text" id="secondNumber" /><br>                                             ^ The id and class attribute values must be in lowercase and split by a dash. (id-class-value)       L16 |     <input type="button" onClick="multiply()" Value="Submit!"/>                                     ^ The attribute name of [ onClick ] must be in lowercase. (attr-lowercase)       L16 |...input type="button" onClick="multiply()" Value="Submit!"/>                                                       ^ The attribute name of [ Value ] must be in lowercase. (attr-lowercase)       L18 |     <img src="elmo.gif"                 ^ Special characters must be escaped : [ < ]. (spec-char-escape)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function multiply() {
-let answer1 = document.getElementById("firstNumber").value;
+let answer1 = document.getElementById("first").value;
 answer1 = parseInt(answer1)
-let answer2 = document.getElementById("secondNumber").value;
+let answer2 = document.getElementById("second").value;
 let answer = 0
 answer = parseInt(answer)
-for (answer2 = document.getElementById("secondNumber").value; answer2 > 0; answer2--) {
+for (answer2 = document.getElementById("second").value; answer2 > 0; answer2--) {
   answer = answer+answer1
 }
 if (answer2 === 0) {
